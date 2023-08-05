44282ff1ec7dfa1337aca244aefb503439d26029
Shilpa Shaji <121919958+shilpashaji1204@users.noreply.github.com>
2023-08-05 18:38:18 -0400

    setup Database

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ require('dotenv').config();
 
 const express = require('express');
 const expresslayouts = require('express-ejs-layouts');
+const connectDB = require('./server/config/db');
+const session = require('express-session');
+const passport = require('passport');
+const MongoStore = require('connect-mongo');
 
 const app = express();
 const port = 5000 || process.env.PORT;
 
+app.use(passport.initialize());
+//app.use(passport.session());
+
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+// Connect to Database
+connectDB();
 
 // Static Files
 app.use(express.static('public'));
@@ -18,6 +28,7 @@ app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
 
 // Routes
+app.use('/', require('./server/routes/auth'));
 app.use('/', require('./server/routes/index'));
 app.use('/', require('./server/routes/dashboard'));
 
